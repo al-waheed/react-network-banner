537a01448157d790da813058a7ce0fbbbfedcd16
Morenikeji <48406420+al-waheed@users.noreply.github.com>
2025-09-11 12:35:57 +0100

    Add demo GIF

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,2 @@
 export { default as NetworkBanner } from "./NetworkBanner";
 export { useNetworkStatus } from "./Hook/useNetworkStatus";
-// export type { NetworkStatus } from "./Hook/useNetworkStatus";
